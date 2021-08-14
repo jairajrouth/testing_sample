4431729337ee34377a43d644685a1609480291e7
jairajrouth <jairajrouth@gmail.com>
2021-08-14 14:09:44 +0200

    Some testing changes for calc suspiciousness score

--- src/SampleProg.py
+++ src/SampleProg.py
@@ -1,21 +1,19 @@
 class JackPot:
     def sum_jack_pot(x, y):
-        if((x - y) >= 1): # Correct is x - y
-            #print("Yes")
+        if(x + y) >= 1:
+            # Correct is x - y
+            # print("Yes")
             return (x - y) * 2
         else:
-        # print("No")
-            return (x - y)
+            # print("No")
+            return x + y
 
-    def greater_value():
-        if 5 > 2:
-            print("5")
 
 if __name__ == "__main__":
-   JackPot.sum_jack_pot(2, 5)
-   JackPot.greater_value()
+    JackPot.sum_jack_pot(2, 5)
 
 
 #mut.py --target SampleProg.py --unit-test tests\test_SampleProg.py -m --report-html DIR_NAME
 #mut.py --target src\SampleProg.py --unit-test tests\test_SampleProg.py -m
 #C:\Users\jaira\AppData\Local\Programs\Python\Python39\mutpy\bin
+#For src not found error. Use "python3 -m" before pytest command
--- tests/test_SampleProg.py
+++ tests/test_SampleProg.py
@@ -7,14 +7,14 @@ class MyTestCase(unittest.TestCase):
     def test_sumJackPot(self):
         self.assertEqual(JackPot.sum_jack_pot(3, 1), 4)
 
-    # def test_sumJackPot2(self): #Fails
-    #     self.assertEqual(JackPot.sum_jack_pot(2, 5), 7)
+    def test_sumJackPot2(self):
+        self.assertEqual(JackPot.sum_jack_pot(2, 5), 7)
 
-    # def test_sumJackPot3(self): #Fails
-    #     self.assertEqual(JackPot.sum_jack_pot(-2, -3), 2)
+    def test_sumJackPot3(self): #Fails
+        self.assertEqual(JackPot.sum_jack_pot(-2, -3), 2)
 
-    def test_sumNoJackPot(self):
-        self.assertEqual(JackPot.sum_jack_pot(5, 1), 8)
+    # def test_sumNoJackPot(self):
+    #     self.assertEqual(JackPot.sum_jack_pot(5, 1), 8)
 
     def test_sumJackPot4(self):
         self.assertEqual(JackPot.sum_jack_pot(4, 0), 8)
